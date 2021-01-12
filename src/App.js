1e6e785e599f28b037a05a88b3c981c22e2bb9de
amanpatniajmer <amanpatniajmer@gmail.com>
2021-01-12 11:38:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import Form from './components/Form';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ContextProvider } from './context/Context';
+import New from './components/New';
+/* import Tree from './Tree'; */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+  /* const Person={
+    name:"",
+    gender:"",
+    imageURL:"",
+    children:[]
+  } */
+
+  function makeNode(person) {
+    let a = (
+      <li key={person.name} className={person.gender.toLowerCase()}>
+        <a href="/">
+          <img src={"./images/" + person.imageURL} className="round-img" alt="" /><br />
+          {person.name}
         </a>
-      </header>
-    </div>
+        {person.children.length > 0 &&
+          <ul>
+            {person.children.map((item) => {
+              console.log(makeNode(item))
+              return makeNode(item)
+            })}
+          </ul>}
+      </li>
+    );
+    return a;
+  }
+  useEffect(() => {
+
+  }, [])
+  return (
+    <Router>
+      <ContextProvider>
+        <Switch>
+          <Route exact path="/add" render={(props)=><Form {...props}/>}/>
+          <Route exact path="/new" render={(props)=><New {...props}/>}/>
+          <div className="App">
+            
+            <div className="tree">
+              <ul>
+                {/* {makeNode({
+            name:"Aman",
+            gender:"Male",
+            imageURL:"aman passport.jpg",
+            children:[{
+              name:"AJ",
+              gender:"Male",
+              imageURL:"aman passport.jpg",
+              children:[]
+            },
+            {
+              name:"Anjali",
+              gender:"Male",
+              imageURL:"aman passport.jpg",
+              children:[]
+            }
+          ]
+          })} */}
+              </ul>
+            </div>
+            <Form />
+          </div>
+        </Switch>
+      </ContextProvider>
+    </Router>
   );
 }
 
